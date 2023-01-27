c87329bf636ea53dbbeb190d09a9c859cff6cce9
Najah7 <najahmatsuo7@gmail.com>
2023-01-27 14:25:14 +0900

    da1 completed

---
+++ day1/practice.py
@@ -0,0 +1,20 @@
+
+# NOTE:stringの例でネックレスを使ってた
+print("Hello world")
+
+string= """
+Day 1 - Python Print function
+The functions is declared like this:
+print('what to print')
+"""
+print(string)
+
+print('line\nanother line\nthe last line')
+
+print("Hello" + " " +"there")
+
+print("Hello " + input("What's your name?: "))
+
+# NOTE:ノートにkey-valueのメモをVariableの例に使っていた
+length = len(input("What's your name?: "))
+print("number of Character in your name:" + str(length))
