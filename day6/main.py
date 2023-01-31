07961dab9035c704d9a1213a59146bbf8435a259
Najah7 <najahmatsuo7@gmail.com>
2023-02-01 08:44:47 +0900

    day6 completed

---
+++ day6/main.py
@@ -0,0 +1,18 @@
+# This lecture for the while loop, functions.
+
+# NOTE:関数の説明にBoxを使っていた。
+
+# NOTE:Reeborg's World：プログラムでロボットを操作するゲーム（https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json）
+# 今日のレッスンでは上記のサイトを使ったので、コード少なめ
+
+# NOTE:インデントについて、ファイルシステムの階層構造を使って説明していた
+
+# NOTE:「space vs indent」（https://www.youtube.com/watch?v=SsoOG6ZeyUI）
+
+# define a function
+def my_fuction():
+    print('Hello')
+    print('Bye')
+    
+# call a function
+my_fuction()
