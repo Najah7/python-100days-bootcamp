96013351771a90d692f3e3b0ffcbae82a1ec10d4
Najah7 <najahmatsuo7@gmail.com>
2023-02-07 20:53:52 +0900

    ✨ day13 completed

--- day13/main.py
+++ day13/main.py
@@ -7,7 +7,7 @@ if number % 2 == 0:
 else:
   print("This is an odd number.")
   
-year = input("Which year do you want to check?")
+year = int(input("Which year do you want to check?"))
 
 if year % 4 == 0:
   if year % 100 == 0:
@@ -22,11 +22,11 @@ else:
   
 
 for number in range(1, 101):
-  if number % 3 == 0 or number % 5 == 0:
+  if number % 3 == 0 and number % 5 == 0:
     print("FizzBuzz")
-  if number % 3 == 0:
+  elif number % 3 == 0:
     print("Fizz")
-  if number % 5 == 0:
+  elif number % 5 == 0:
     print("Buzz")
   else:
     print([number])
