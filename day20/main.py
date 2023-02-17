0a6e946efa7fd367494a9ea5eed89e3d5db36aa1
Najah7 <najahmatsuo7@gmail.com>
2023-02-17 16:25:09 +0900

    🚧 簡易的にSnakeクラスを作成

---
+++ day20/main.py
@@ -0,0 +1,59 @@
+# Project Snake Game (first half)
+
+from turtle import Screen, Turtle
+import time
+
+from snake import Snake
+
+
+def main():
+    screen = init_screen()
+    
+    snake = Snake()
+    
+    snake.setup_snake()
+    
+    screen.update()
+    # 無限ループしない用のカウンタ
+    counter_for_dev = 0
+    
+    while True:
+        
+        # 再レンダリングと少し動きに間を
+        # NOTE:0.2秒に特に意味はない、見た感じの直感で
+        screen.update()
+        time.sleep(0.2)
+        
+        # とりあえず全身させている。
+        snake.forward()
+        
+        
+        # NOTE:無限ループしないために
+        counter_for_dev += 1
+        if counter_for_dev == 10: break
+            
+        
+
+    
+    screen.exitonclick()
+    
+    
+
+def init_screen():
+    # initialize a screen
+    screen = Screen()
+    # set size of the screen
+    screen.setup(width=600, height=600)
+    # set color of the screen
+    screen.bgcolor("black")
+    # set title of the game
+    screen.title('Snake Game🐍')
+    # updateのタイミングで画面を描写するための設定
+    # NOTE:説明ではブラウン管モニターやGIFアニメーション（パラパラ漫画的な、静止画を素早く切り替えて動画に）などを例に説明していた
+    screen.tracer(0)
+    
+    return screen
+        
+
+if __name__ == '__main__':
+    main()
---
+++ day20/snake.py
@@ -0,0 +1,51 @@
+from turtle import Turtle
+
+# NOTE:クラスの外でOK？
+SIZE_OF_SQUARE = 20
+MOVE_DISTANCE = 20
+
+
+class Snake:
+    
+    def __init__(self, snake_head_position=[0, 0], size_of_snake=3) -> None:
+        self.snake = [Turtle('square') for _ in range(size_of_snake)]
+        self.head_position_x = snake_head_position[0]
+        self.head_position_y = snake_head_position[1]
+        
+
+    def setup_snake(self, color='white'):
+        
+        position_x = self.head_position_x
+        position_y = self.head_position_y
+        for snake_body in self.snake:
+            snake_body.color(color)
+            snake_body.penup()
+            snake_body.goto(x=position_x, y=position_y)
+            position_x -= SIZE_OF_SQUARE
+    
+    def forward(self):
+        self.move()
+        self.snake[0].forward(MOVE_DISTANCE)
+    
+    def backward(self):
+        self.move()
+        self.snake[0].backward(MOVE_DISTANCE)
+        
+    def left(self):
+        self.move()
+        self.snake[0].left(90)
+        self.snake[0].forward(MOVE_DISTANCE)
+    
+    def right(self):
+        self.move()
+        self.snake[0].right(90)
+        self.snake[0].forward(MOVE_DISTANCE)
+    
+    # HACK:命名
+    def move(self):
+        # スネークの体の一部（タートル）のしっぽ側を一つ頭側に動かす処理
+        # NOTE:スネークの配列は「頭→しっぽ」という風に並んだ配列
+        for snake_body_num in range(len(self.snake) - 1, 0, -1):
+            nex_x = self.snake[snake_body_num - 1].xcor()
+            nex_y = self.snake[snake_body_num - 1].ycor()
+            self.snake[snake_body_num].goto(x=nex_x, y=nex_y)    
