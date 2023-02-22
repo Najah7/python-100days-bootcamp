4ab46fae83bf09beb7e86bd57c8db81f8b2196a1
Najah7 <najahmatsuo7@gmail.com>
2023-02-22 13:15:05 +0900

    ♻️ モジュール化

--- day23/my_original_version/main.py
+++ day23/my_original_version/main.py
@@ -8,50 +8,55 @@ from scoreboard import Scoreboard
 CAR_SPEED = 0.1
 COLLISION_DISTANCE = 22
 
-# Screen setup
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.tracer(0)
 
-# initialize compoments used in this game
-player = Player()
-scoreboard = Scoreboard()
-car_manager = CarManager()
- 
-game_is_on = True
-while game_is_on:
+def main():
+    # Screen setup
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.tracer(0)
+
+    # initialize compoments used in this game
+    player = Player()
+    scoreboard = Scoreboard()
+    car_manager = CarManager()
     
-    # rendering setup
-    time.sleep(CAR_SPEED)
-    screen.update()
-    
-    # move car from right to left
-    car_manager.go_left()
-    
-    # handle user input
-    screen.listen()
-    screen.onkey(player.go_up, 'Up')
-    screen.onkey(player.go_down, 'Down')
-    
-    # increase Score and get player back to start position  when player get to the goal
-    if player.is_at_goal():
-        scoreboard.increase_score()
-        scoreboard.update_score_board()
-        player.back_to_start()
-    
-    # Detect collsion with cars
-    for car in car_manager.cars:
-        # HACK:車にあたった時の検知する距離感。（横が長くて、縦が短いので、値を大きくすると横を通りすぎただけで衝突した検知してしまう。）
-        if player.distance(car) < COLLISION_DISTANCE:
-            
-            # NOTE：衝突検知の距離感を調整する際に使うと便利なprint （微調整用に残している）
-            # print(player.distance(car))
-            
-            # print game over  
-            scoreboard.print_game_over()
-            game_is_on = False
+    game_is_on = True
+    while game_is_on:
         
+        # rendering setup
+        time.sleep(CAR_SPEED)
+        screen.update()
+        
+        # move car from right to left
+        car_manager.go_left()
+        
+        # handle user input
+        screen.listen()
+        screen.onkey(player.go_up, 'Up')
+        screen.onkey(player.go_down, 'Down')
+        
+        # increase Score and get player back to start position  when player get to the goal
+        if player.is_at_goal():
+            scoreboard.increase_score()
+            scoreboard.update_score_board()
+            player.back_to_start()
+        
+        # Detect collsion with cars
+        for car in car_manager.cars:
+            # HACK:車にあたった時の検知する距離感。（横が長くて、縦が短いので、値を大きくすると横を通りすぎただけで衝突した検知してしまう。）
+            if player.distance(car) < COLLISION_DISTANCE:
+                
+                # NOTE：衝突検知の距離感を調整する際に使うと便利なprint （微調整用に残している）
+                # print(player.distance(car))
+                
+                # print game over  
+                scoreboard.print_game_over()
+                game_is_on = False
+            
 
-screen.exitonclick()
+    screen.exitonclick()
+
+if __name__ == '__main__':
+    main()
 
 
