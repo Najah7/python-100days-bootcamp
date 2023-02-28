23911a611f942a7467540cdc396b23b4e009abb0
Najah7 <najahmatsuo7@gmail.com>
2023-02-28 19:47:27 +0900

    ✨ day30のExceptionの練習

--- day26/NATO-alphabet-start/main.py
+++ day26/NATO-alphabet-start/main.py
@@ -9,12 +9,18 @@ dict_nato_phonetic_alphabets = {row.letter: row.code for (index, row) in df_nato
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# get user input
-input_word = input('Enter a word: ').upper()
-# make letters list from user name
-letters = [letter for letter in input_word]
-# output NATO phonetic alphabet on each letter
-for letter in letters:
-    output_word = dict_nato_phonetic_alphabets[letter]
-    print(f"{letter}: {output_word}")
+while True:
+    # get user input
+    input_word = input('Enter a word: ').upper()
+    # make letters list from user name
+    letters = [letter for letter in input_word]
+    # output NATO phonetic alphabet on each letter
+    try:
+        for letter in letters:
+            output_word = dict_nato_phonetic_alphabets[letter]
+            print(f"{letter}: {output_word}")
+        break
+    except KeyError:
+        print('Sorry, Only litters in the alphabet please')
+        continue
 
