d912a6a952923f4672a24dc23a2ff679fdbf2dd7
Najah7 <najahmatsuo7@gmail.com>
2023-03-19 16:26:14 +0900

    ✨ skip day47

---
+++ day47/main.py
@@ -0,0 +1,35 @@
+"""
+Project Amazon Price Tracker
+"""
+
+# NOTE:Amazonのスクレーピング対策がなかなかで、短時間で解決できそうにないので、諦め。
+#        シンプルにAPIの勉強として、AmazonのAPIを使ってなら、簡単に実装できそう。
+
+from bs4 import BeautifulSoup
+import requests
+import lxml
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# NOTE:URLに埋め込む形のToken認証なので、URLを公開していいかわからなかったので、環境変数に
+url = os.getenv('URL')
+
+"""
+HACK:URLにトークンを埋め込むタイプなので、スクレーピングは難しめ（多分JWTなのかな...）
+    動画ではスクレーピングしてたけど、Amazon Product Advertising APIを使うのをおすすめ（bottlenose使って）
+"""
+headers = {
+    'Accept-Language': 'ja,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
+    'Uesr-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.44'
+}
+
+res = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(res.content, "lxml")
+
+print(soup.prettify())
+
+price = soup.find(name='span', class_='a_price_whole')
+print(price)
